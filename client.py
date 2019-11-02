10f0213bb18303ca2c15a6f14ec265adc1805052
Wachlan <52527188+Wachlan@users.noreply.github.com>
2019-11-02 12:55:56 +1100

    Threading for Client

--- client.py
+++ client.py
@@ -7,10 +7,14 @@
 
 import sys  #contains the command-line arguments passed to the script
 import socket
+import threading
+import time
 
 serverIP = str(sys.argv[1])
 serverPort = int(sys.argv[2])
 
+
+"""
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -35,31 +39,53 @@ sock.send(loginDetails)
 
 loginResult = sock.recv(1024)
 print(loginResult)
+"""
+
+def recv_handler():
+    username = raw_input('Username:')
+#raw_input() is a built-in function in Python. When this command is executed, the user at the client is prompted with
+#the words ?Username:? The user then uses the keyboard to input a line, which is put into the 
+#variable sentence. Now that we have a socket and a message, we will want to send the message through the socket
+#to the destination host.
+
+    password = raw_input('Password:')
+
+    loginDetails = username + " " + password
+
+    sock.send(loginDetails)
+#As the connection has already been established, the client program simply drops the bytes in the string sentence 
+#into the TCP connection. Note the difference between UDP sendto() and TCP send() calls. In TCP we do not need to 
+#attach the destination address to the packet, as was the case with UDP sockets.
+
+    while(1):
+        infoReceived = sock.recv(1024)
+        print(infoReceived)
+        #print("rec handler loop")
 
 
- 
+def send_handler():
+    time.sleep(3)
+    while(1):
+        command = raw_input('Command:')
+        sock.send(command)
+        #print("send handler loop")
 
 
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Connect the socket to the port where the server is listening
+server_address = (serverIP, serverPort)
+sock.connect(server_address)
 
+recv_thread=threading.Thread(name="RecvHandler", target=recv_handler)
+recv_thread.daemon=True
+recv_thread.start()
 
+send_thread=threading.Thread(name="SendHandler",target=send_handler)
+send_thread.daemon=True
+send_thread.start()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#this is the main thread
+while True:
+    time.sleep(0.1)
